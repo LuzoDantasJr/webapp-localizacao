bcc905f5a4422ca35c8ee819f857cb3627147a16
LuzoDantasJr <luzo.dantas.jr@gmail.com>
2025-07-24 11:54:34 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,20 @@
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open('webapp-cache-v1').then(function (cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
-      ]);
-    })
+const CACHE_NAME = 'monitoramento-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
